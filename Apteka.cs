a0a65ef4ed6e3dcfec0995bede3a9d7d761f05ef
Вика Караева <vikakaraevavika@gmail.com>
2022-02-28 14:51:03 +0300

    Добавьте файлы проекта.

---
+++ Apteka.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sr1
+{
+    class Apteka
+    {
+        public string name; //название аптеки
+        public string name_l; //название лекарств
+        public int money_l; //стоимость каждого лекарства
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="name_l"></param>
+        /// <param name="money_l"></param>
+
+        //описание методов
+        public Apteka(string name, string name_l, int money_l)
+        {
+            this.name = name;
+            this.name_l = name_l;
+            this.money_l = money_l;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Название аптеки{name},название лекарств{name_l}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. инициализировать все поля
+            //2. получить полную информацию об аптеке
+            //3. выяснить стоимость самого дорогого лекарства
+            //4. найти стоимость всех лекарсив
+
+            Console.WriteLine("Демонстрация работы класса:");
+            List <Apteka> apteka = new List <Apteka>();
+            Console.WriteLine("введите название аптеки:");
+            string name = Console.ReadLine();
+            Console.WriteLine("кол-во лекарств:");
+            int kol = Convert.ToInt32(Console.ReadLine());
+
+            for (int x=0;x < kol;x++)
+            {
+                Console.WriteLine("введите название лекарства: ");
+                string name_l = Console.ReadLine();
+                Console.WriteLine("введите его цену: ");
+                int money_l = Convert.ToInt32(Console.ReadLine());
+                apteka.Add(new Apteka(name, name_l, money_l));
+            }
+            apteka.Sort();
+            foreach (Apteka z in apteka)
+                z.Info();
+        }
+    }
+}
